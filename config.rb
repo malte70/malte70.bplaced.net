e85d28db7c6f86e016042796fc67a033c634146d
Malte Bublitz <malte70+github@mcbx.de>
2016-05-26 16:35:49 +0200

    First version coded in a sleepless night ;-)

---
+++ config.rb
@@ -0,0 +1,19 @@
+#
+# Compass configuration
+#
+
+http_path       = "htdocs/"
+css_dir         = http_path + "assets/css"
+sass_dir        = http_path + "assets/scss"
+images_dir      = http_path + "assets/img"
+javascripts_dir = http_path + "assets/js"
+
+# Possible values:
+#  - :expanded
+#  - :nested
+#  - :compact
+#  - :compressed
+output_style = :compressed
+
+# No debugging comments
+line_comments = false
